25d7b342f2a1a8da4a664fdf0d3ee69df242997d
dgarciac <davinia.garcia@opentrends.net>
2018-02-09 12:21:45 +0100

    - Publicity and countdown

--- js/custom.js
+++ js/custom.js
@@ -36,7 +36,7 @@ jQuery(function($) {
 
 	if ( $( '.countdown' ).length > 0 ) {
 		$(".countdown").jCounter({
-		  	date: '21 October 2018 12:00:00',
+		  	date: '10 March 2018 00:00:00',
 		  	fallback: function() { console.log("count finished!") }
 		});
 	}
