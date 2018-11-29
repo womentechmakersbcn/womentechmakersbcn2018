0c0034698b56648cd78a878b801258830def70db
daviniagarcia <daviniaitig@gmail.com>
2018-11-29 20:08:54 +0100

    C4P 2019

--- js/custom.js
+++ js/custom.js
@@ -36,7 +36,7 @@ jQuery(function($) {
 
 	if ( $( '.countdown' ).length > 0 ) {
 		$(".countdown").jCounter({
-		  	date: '10 March 2018 00:00:00',
+		  	date: '6 April 2019 00:00:00',
 		  	fallback: function() { console.log("count finished!") }
 		});
 	}
